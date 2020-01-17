8b560ff97a126253307666be0f5103dfeb1f3bfc
Andrii Cherevko <cherevko_andrei@i.ua>
2020-01-17 15:05:20 +0200

    added play function

--- js/index.js
+++ js/index.js
@@ -15,9 +15,9 @@ function getRecomendations(Rating = 0) {
     }
     case 4: {
       return "Wasted time";
-     }
+    }
     case 5: {
-      return "If you haven\'t other variants";
+      return "If you haven't other variants";
     }
     case 6: {
       return "Not bad";
@@ -36,5 +36,28 @@ function getRecomendations(Rating = 0) {
     }
     default:
       return "You need to specify rating from 1 to 10";
+  }
+}
+
+function play(duration, song, repeatPeriod) {
+  duration = Math.round(Number(duration));
+  repeatPeriod = Math.round(Number(repeatPeriod));
+
+  if (!duration) {
+    alert("Need to specify duration");
+    return;
+  } else if (!repeatPeriod) {
+    alert("Need to specify repeat period");
+    return;
+  } else if (duration < repeatPeriod) {
+    alert("Duration must be more then repeat period");
+    return;
+  }
+
+  for (let i = 1; i < duration; i++) {
+    if (i > repeatPeriod && i % repeatPeriod == 1) {
+      console.log(song);
+    }
+    console.log(i);
   }
 }
